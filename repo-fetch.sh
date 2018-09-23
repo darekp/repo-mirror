6bc32b2dec7bfffdf00651ae01891f2e1c06b167
Dariusz Pelowski <dariusz.pelowski@gmail.com>
2018-09-23 17:35:03 +0200

    don't prompt on the terminal for git passwords

--- repo-fetch.sh
+++ repo-fetch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 set -eu
+export GIT_TERMINAL_PROMPT=0
 
 find $REPO_MIRROR_LOCATION -path "*.git/config"  | xargs -P 4 --replace bash -c 'cd $(dirname {}) && pwd &&  git fetch --all -p -t'
